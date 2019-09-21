6f55ce7c628d7f28a8b5c263e8a27f81575946da
johnchrisdc <johnchrisdelacruz@gmail.com>
2019-09-21 11:16:09 +0800

    Added support for GitHub Gist

--- github.js
+++ github.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-	var logos = document.getElementsByClassName("octicon-mark-github");
+	var logos = document.getElementsByClassName(location.href.includes("gist.github") ? "octicon-logo-github" : "octicon-mark-github");
 	var logo_src = chrome.extension.getURL("images/github.png");
 
 	for(var i = 0; i < logos.length; i++){
@@ -7,12 +7,18 @@ window.onload = function() {
 			logos[i].removeChild(logos[i].firstChild);
 		}
 
-		var img=document.createElement("img");
-		img.src=logo_src;
+		var img = document.createElement("img");
+		img.src = logo_src;
 		img.style.height = "32px";
 		img.className = "v-align-middle";
 
-		logos[i].parentNode.appendChild(img);
+		if (location.href.includes("gist.github")) {
+			logos[i].parentNode.prepend(img);
+		} else {
+			logos[i].parentNode.appendChild(img);
+		}
+
 		logos[i].remove();
 	}
+
 }
